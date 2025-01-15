cffb48439fd8b0ad3bafacb91cbdbdf229493223
Jonas Gann <jonas@gann.digital>
2025-01-15 13:48:12 +0100

    add cropper for perspective adjustment

--- classifier-app/src/App.jsx
+++ classifier-app/src/App.jsx
@@ -1,21 +1,43 @@
 import './App.css'
 
-import { useState } from 'react'
+import { useState, useCallback, useRef } from 'react'
 import ort from "onnxruntime-web";
 import { Camera, CameraResultType } from '@capacitor/camera';
 import * as utils from './utils.js'
 import vocab from './assets/vocab.json'
 import modelPath from './assets/model.onnx'
 import test_image from './assets/test_image.png'
+import Cropper from 'react-document-crop'
 
 
 function App() {
   
-  const [image, setImage] = useState(test_image)
   const [probImma, setProbImma] = useState("---")
   const [probZert, setProbZert] = useState("---")
+  const cropperRef = useRef()
+  const [cropState, setCropState] = useState()
+  const [image, setImage] = useState(test_image)
+  const [cropperKey, setCropperKey] = useState(0)
+  const onDragStop = useCallback((s) => setCropState(s), [])
+  const onChange = useCallback((s) => setCropState(s), [])
+  const [isCropped, setIsCropped] = useState(false)
+
+  const filterCvParams = {
+    blur: false,                         // Applies a Gaussian blur to the image.
+    //th: true,                            // Applies adaptive thresholding to the image.
+    //thMode: cv.ADAPTIVE_THRESH_MEAN_C,   // Determines the method used for adaptive thresholding.
+    //thMeanCorrection: 15,                // Adjusts the mean for thresholding.
+    //thBlockSize: 25,                     // Determines the size of the block for adaptive thresholding.
+    //thMax: 255,                          // Sets the maximum value for thresholding.
+    grayScale: true                      // Converts the image to grayscale.
+  }
+
 
   async function run_model() {
+   
+    setProbImma("---")
+    setProbZert("---")
+
     const vocab_map = new Map(vocab)
     const png_text = await utils.extract_text_from_png(image)
     const sample_tokens = utils.tokenize_text(png_text)
@@ -31,6 +53,12 @@ function App() {
 
   }
 
+  const handleCrop = async () => {
+    const croppedImage = await cropperRef.current.done({preview: true, filterCvParams})
+    setImage(croppedImage)
+    setIsCropped(true)
+  }
+
 
   const takePicture = async () => {
 
@@ -40,23 +68,35 @@ function App() {
       resultType: CameraResultType.DataUrl
     });
 
+    setCropperKey(prev => prev + 1)
+
     setImage(image.dataUrl);
- 
+    setIsCropped(false)
   };
 
   return (
     <>
       <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-        <img src={image} style={{ width: '100%', maxWidth: '400px', height: 'auto', margin: '0 auto' }}/>
-        <button onClick={takePicture}>Take Picture</button>
-        <button onClick={run_model}>Run Model</button>
+        <div style={{ maxWidth: '400px', margin: '0 auto', width: '100%' }}>
+          <Cropper
+            ref={cropperRef}
+            image={image}
+            onChange={onChange}
+            onDragStop={onDragStop}
+            key={cropperKey}
+          />
+        </div>
+        <div style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
+          <button onClick={takePicture}>Take Picture</button>
+          <button onClick={handleCrop} disabled={isCropped}>Crop Image</button>
+          <button onClick={run_model}>Run Model</button>
+        </div>
         <div style={{ border: '1px solid black', padding: '10px', borderRadius: '5px', minHeight: '100px' }}>
           <p>Wahrscheinlichkeit einer Immatrikulation:</p>
           <p>{probImma}</p>
           <p>Wahrscheinlichkeit eines Zertifikats:</p>
           <p>{probZert}</p>
         </div>
-
       </div>
     </>
   )
