7ddd2b9773aa3e6ea9a4b8188941e3f67947a436
Jonas Gann <jonas@gann.digital>
2025-01-17 12:11:42 +0100

    update pyodide demo

--- classifier-app-pyodide/src/App.jsx
+++ classifier-app-pyodide/src/App.jsx
@@ -9,6 +9,7 @@ import test_image from './assets/test_image.png'
 import { loadPyodide } from './pyodide/pyodide.mjs'
 import CodeMirror from '@uiw/react-codemirror';
 import { python } from '@codemirror/lang-python';
+import frog from './assets/frog.webp'
 
 function App() {
   const initialCode = `
@@ -23,7 +24,8 @@ print(array)
 `
 
   const [code, setCode] = useState(initialCode);
-  const [pipPackages, setPipPackages] = useState(['snowballstemmer', 'numpy']);
+  const [pipPackages, setPipPackages] = useState(['snowballstemmer']);
+  const [pyodidePackages, setPyodidePackages] = useState(['numpy']);
   const [output, setOutput] = useState('');
   const [pyodide, setPyodide] = useState(null);
 
@@ -37,6 +39,10 @@ print(array)
 
   async function run_python() {
     await pyodide.loadPackage("micropip", {checkIntegrity: false});
+    for (const pipPackage of pyodidePackages) {
+      await pyodide.loadPackage(pipPackage);
+    }
+
     const micropip = pyodide.pyimport("micropip");
     for (const pipPackage of pipPackages) {
       await micropip.install(pipPackage);
@@ -49,6 +55,28 @@ print(array)
 
   return (
     <>
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '20px',
+      }}>
+        <img
+          src={frog}
+          style={{
+            height: '50px',
+            marginRight: '15px'
+          }}
+        />
+        <h1 style={{
+          margin: 0,
+          fontSize: '24px',
+
+        }}>
+          Web Python
+        </h1>
+      </div>
+
     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
     <CodeMirror
         value={code}
@@ -60,6 +88,7 @@ print(array)
         style={{textAlign: 'left'}}
       />
 
+      <p>Pyodide Packages: <input type="text" value={pyodidePackages.join(',')} onChange={(e) => setPyodidePackages(e.target.value.split(','))}></input></p>
       <p>Pip Packages: <input type="text" value={pipPackages.join(',')} onChange={(e) => setPipPackages(e.target.value.split(','))}></input></p>
       <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
         <button onClick={load_pyodide} disabled={pyodide}>Load Pyodide</button>
